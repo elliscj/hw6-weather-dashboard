197c73e37a136b4661a41ccf3a6bcb4b6fb35093
Cam Ellis <elliscj@colorado.edu>
2021-11-19 23:47:55 -0700

    starting function/css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,38 @@
-document.querySelector(".searchBtn").addEventListener("click", function () {
-  console.log("hi");
-});
-var searchLocation = document.getElementById("search-input").value;
-apiUrl = "";
+//"https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly,alerts&appid=e21bef52f4595a9117145774881d361c"
+https: var searchCity = "";
+var apiCity = "";
+var apiKey = "e21bef52f4595a9117145774881d361c";
+var futureUrl =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+  lat +
+  "&lon=" +
+  lon +
+  "&exclude=minutely,hourly,alerts&appid=" +
+  apiKey;
 
 var currentDay = moment().format("MMM Do YY");
 
 console.log(currentDay);
 
-fetch(apiUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    //for loop to populate 5-day cards
-    //moment for formatting dates from api
-  });
+document.querySelector(".searchBtn").addEventListener("click", function () {
+  searchCity = document.getElementById("search-input").value;
+  apiCity = searchCity.trim();
+  var currentUrl =
+    "api.openweathermap.org/data/2.5/weather?q=" +
+    apiCity +
+    "&units=imperial&appid=" +
+    apiKey;
+
+  console.log("hi");
+  console.log(searchCity);
+  console.log(futureUrl);
+  console.log(apiCity);
+  fetch(cfutureUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //for loop to populate 5-day cards
+      //moment for formatting dates from api
+    });
+});
